c72ee33894daa59a08705085efe4906406c96bb5
Yinan <nickycarving@gmail.com>
2016-02-25 23:04:32 -0800

    Initial Commit

---
+++ CodePath-wk4-Tumblr/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CodePath-wk4-Tumblr
+//
+//  Created by Yinan iMac on 2/25/16.
+//  Copyright © 2016 YinanLearningSwift. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
